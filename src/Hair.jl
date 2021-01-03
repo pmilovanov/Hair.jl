149b49debe9dacf79fe831b86328e364a3b28aec
Paul Milovanov <paul@dada.science>
2021-01-03 11:35:15 -0500

    skeleton of test for gendata

--- src/Hair.jl
+++ src/Hair.jl
@@ -2,5 +2,8 @@ module Hair
 
 include("synthetic.jl")
 include("img.jl")
+include("gendata.jl")
+
+export sample_image, GridStrategy, RandomPositionStrategy
 
 end
---
+++ test/test_gendata.jl
@@ -0,0 +1,16 @@
+using Images
+using Hair
+H = Hair
+
+using Test
+
+const GA = GrayA
+
+H = Hair
+
+@testset "Generating synthetic image training data" begin
+
+    
+
+    
+end
