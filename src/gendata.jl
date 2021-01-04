ecd92f718601c2040d0c87509a9b1d6e30f3e585
Paul Milovanov <paul@dada.science>
2021-01-03 22:18:54 -0500

    one test and some changes for sample_image with GridStrategy

--- src/gendata.jl
+++ src/gendata.jl
@@ -1,8 +1,10 @@
 # Functions to generate training data using stuff from img.jl
 
+using StatsBase
+
 abstract type SamplingStrategy end
 
-sample_image(img::Image{T}, s::S)::Array{Image{T},1} where {T, S<:SamplingStrategy} = error("Not implemented")
+sample_image(img::Image{T}, s::S) where {T,S<:SamplingStrategy} = error("Not implemented")
 
 struct GridStrategy <: SamplingStrategy
     nsamples::Int
@@ -13,17 +15,18 @@ end
 
 function sample_image(img::Image, s::GridStrategy)
     if any(size(img) .< s.sidelength); return []; end
-    
-    times_fully_fits_into_length(l_given, l_into, overlap) = ((l_into - l_given)÷overlap + 1)
+
+    times_fully_fits_into_length(l_into, l_given, overlap) =
+        ((l_into - l_given) ÷ (l_given - overlap) + 1)
     nfits_x = times_fully_fits_into_length(size(img)[1], s.sidelength, s.overlap)
     nfits_y = times_fully_fits_into_length(size(img)[2], s.sidelength, s.overlap)
 
-    candidates = [(1,1) .+ (s.overlap*(i-1), s.overlap*(j-1)) for i=1:nfits_x for j=1:nfits_y]
-    samples = candidates
-    
-    if nsamples != length(candidates)
-        # sample from candidates
-    end
+    step = s.sidelength - s.overlap
+    candidates =
+        [(1, 1) .+ (step * (i - 1), step * (j - 1)) for i = 1:nfits_x for j = 1:nfits_y]
+    samples =
+        (s.nsamples == length(candidates)) ? samples :
+        [sample(candidates) for i = 1:s.nsamples]
 
     [img[c[1]:c[1]+s.sidelength-1, c[2]:c[2]+s.sidelength-1] for c in samples]
 end
@@ -33,8 +36,3 @@ struct RandomPositionStrategy <: SamplingStrategy
     nsamples::Int
     sidelength::Int
 end
-
-
-
-    
-
--- test/test_gendata.jl
+++ test/test_gendata.jl
@@ -8,9 +8,18 @@ const GA = GrayA
 
 H = Hair
 
+testdata(datasize::Int) =
+    reshape([i + j for i = 1:datasize for j = 1:datasize], (datasize, datasize))
+
 @testset "Generating synthetic image training data" begin
 
-    
+    img = testdata(6)
 
-    
+    @test [
+        [2 3 4; 3 4 5; 4 5 6],
+        [4 5 6; 5 6 7; 6 7 8],
+        [4 5 6; 5 6 7; 6 7 8],
+        [6 7 8; 7 8 9; 8 9 10],
+    ] == H.sample_image(img, GridStrategy(4, 3, 1))
+
 end
