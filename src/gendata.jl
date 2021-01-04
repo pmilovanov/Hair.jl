7d63ea46ba3c388fe0fbfea4daa0e7dea065ffe1
Paul Milovanov <paul@dada.science>
2021-01-04 13:55:38 -0500

    checkpoint

--- src/gendata.jl
+++ src/gendata.jl
@@ -1,38 +1,43 @@
 # Functions to generate training data using stuff from img.jl
 
 using StatsBase
+using Base: @kwdef
 
 abstract type SamplingStrategy end
 
 sample_image(img::Image{T}, s::S) where {T,S<:SamplingStrategy} = error("Not implemented")
 
-struct GridStrategy <: SamplingStrategy
-    nsamples::Int
-    sidelength::Int
-    overlap::Int
+@kwdef struct GridStrategy <: SamplingStrategy
+    n::Int = 0
+    side::Int = 128
+    overlap::Int = 10
+    cover_edges::Bool = False
 end
 
 
 function sample_image(img::Image, s::GridStrategy)
-    if any(size(img) .< s.sidelength); return []; end
+    if any(size(img) .< s.side); return []; end
 
     times_fully_fits_into_length(l_into, l_given, overlap) =
         ((l_into - l_given) ÷ (l_given - overlap) + 1)
-    nfits_x = times_fully_fits_into_length(size(img)[1], s.sidelength, s.overlap)
-    nfits_y = times_fully_fits_into_length(size(img)[2], s.sidelength, s.overlap)
+    nfits_x = times_fully_fits_into_length(size(img)[1], s.side, s.overlap)
+    nfits_y = times_fully_fits_into_length(size(img)[2], s.side, s.overlap)
 
-    step = s.sidelength - s.overlap
+    step = s.side - s.overlap
     candidates =
         [(1, 1) .+ (step * (i - 1), step * (j - 1)) for i = 1:nfits_x for j = 1:nfits_y]
-    samples =
-        (s.nsamples == length(candidates)) ? samples :
-        [sample(candidates) for i = 1:s.nsamples]
 
-    [img[c[1]:c[1]+s.sidelength-1, c[2]:c[2]+s.sidelength-1] for c in samples]
+#    if s.cover_edges && (step*nfits_x
+    
+    samples =
+        (s.n == 0 || s.n == length(candidates)) ? samples :
+        [sample(candidates) for i = 1:s.n]
+
+    [img[c[1]:c[1]+s.side-1, c[2]:c[2]+s.side-1] for c in samples]
 end
 
 
 struct RandomPositionStrategy <: SamplingStrategy
-    nsamples::Int
-    sidelength::Int
+    n::Int
+    side::Int
 end
--- test/test_gendata.jl
+++ test/test_gendata.jl
@@ -13,13 +13,27 @@ testdata(datasize::Int) =
 
 @testset "Generating synthetic image training data" begin
 
-    img = testdata(6)
+    img = [2  3  4   5   6   7
+           3  4  5   6   7   8
+           4  5  6   7   8   9
+           5  6  7   8   9  10
+           6  7  8   9  10  11
+           7  8  9  10  11  12]
 
-    @test [
-        [2 3 4; 3 4 5; 4 5 6],
-        [4 5 6; 5 6 7; 6 7 8],
-        [4 5 6; 5 6 7; 6 7 8],
-        [6 7 8; 7 8 9; 8 9 10],
-    ] == H.sample_image(img, GridStrategy(4, 3, 1))
+    
+    @test H.sample_image(img, GridStrategy(n=4, side=3, overlap=1)) == [
+        [2 3 4
+         3 4 5
+         4 5 6],
+        [4 5 6
+         5 6 7
+         6 7 8],
+        [4 5 6
+         5 6 7
+         6 7 8],
+        [6 7 8
+         7 8 9
+         8 9 10],
+    ]
 
 end
